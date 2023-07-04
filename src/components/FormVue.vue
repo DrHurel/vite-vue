750cba3064675e71265a7e9e6175c042798e3753
DrHurel <jeremyhurel34@gmail.com>
2023-07-04 10:43:59 +0200

    save

--- src/components/FormVue.vue
+++ src/components/FormVue.vue
@@ -19,12 +19,15 @@
 
 <template>
 
-  <section v-if="isValid">
-    <!-- Success message start -->
+  <section v-if="isValid" class="confirmationMessage">
+    <img src="../assets/images/icon-success.svg" alt="">
+
     <h1>Thanks for subscribing!</h1>
     <p>
       A confirmation email has been sent to
-    ash@loremcompany.com. Please open it and click the button inside to confirm
+    <strong>ash@loremcompany.com</strong>
+    
+      . Please open it and click the button inside to confirm
     your subscription. 
     </p>
     <button @click="reset">
